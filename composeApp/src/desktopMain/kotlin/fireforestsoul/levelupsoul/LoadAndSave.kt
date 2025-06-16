11b5ea58654bf02a8f1c9d61f9fc3bcfc0180bfc
Fire Forest Souls <stepan.sniff@gmail.com>
2025-06-16 14:18:12 +0300

    alpha0.1 Desktop save and load for habits
    🖥️ Реализовано сохранение привычек на Desktop
    📤 Добавлена загрузка привычек на Desktop

--- composeApp/src/desktopMain/kotlin/fireforestsoul/levelupsoul/LoadAndSave.kt
+++ composeApp/src/desktopMain/kotlin/fireforestsoul/levelupsoul/LoadAndSave.kt
@@ -1,16 +1,63 @@
 package fireforestsoul.levelupsoul
 
+import kotlinx.datetime.LocalDate
 import java.io.File
 
-val file = File(save_file_name)
-
 actual fun saveValue() {
-    file.writeText(app_version.toString())
+    File(save_file_name).printWriter().use { out ->
+        out.println(app_version.toString())
+        out.println(habits.size.toString())
+        for (x in 0 until habits.size) {
+            out.println(habits[x].nameOfHabit)
+            out.println(habits[x].nameOfUnitsOfDimension)
+            out.println(habits[x].typeOfGoalHabits.toString())
+            out.println(habits[x].needGoal.toString())
+            out.println(habits[x].needDays.toString())
+            out.println(habits[x].startDate.toString())
+            out.println(habits[x].lastDate.toString())
+            out.println(habits[x].habitDay.size.toString())
+            for (y in 0 until habits[x].habitDay.size) {
+                out.println(habits[x].habitDay[y].today.toString())
+                out.println(habits[x].habitDay[y].totalOfAPeriod.toString())
+                out.println(habits[x].habitDay[y].correctly.toString())
+            }
+        }
+    }
 }
 
 actual fun loadValue() {
-    val oldAppVersion: Long = if (file.exists()) file.readText().toLong() else app_version
-//    if (oldAppVersion) {
-//        loading old type
-//    }
+    val file = File(save_file_name)
+    if (file.exists()) {
+        val input = file.readLines()
+        
+        val oldAppVersion = input.getOrNull(0)?.toLong()
+        if (oldAppVersion != null) {
+            if (oldAppVersion > 0) {
+                val habitsSize = input.getOrNull(1)?.toInt()!!
+                habits = mutableListOf(Habit())
+                var plusIndexX = 0
+                for (x in 0 until habitsSize) {
+                    if (x > 0) habits.add(Habit())
+                    habits[x].nameOfHabit = input.getOrNull(2 + plusIndexX).toString()
+                    habits[x].nameOfUnitsOfDimension = input.getOrNull(3 + plusIndexX).toString()
+                    habits[x].typeOfGoalHabits = enumValueOf<TypeOfGoalHabits>(input.getOrNull(4 + plusIndexX).toString())
+                    habits[x].needGoal = input.getOrNull(5 + plusIndexX)?.toDouble()!!
+                    habits[x].needDays = input.getOrNull(6 + plusIndexX)?.toInt()!!
+                    habits[x].startDate = input.getOrNull(7 + plusIndexX)?.let { LocalDate.parse(it) }!!
+                    habits[x].lastDate = input.getOrNull(8 + plusIndexX)?.let { LocalDate.parse(it) }!!
+                    val habitDaySize = input.getOrNull(9 + plusIndexX)?.toInt()!!
+                    habits[x].habitDay = mutableListOf(HabitDay())
+                    var plusIndexY = 0
+                    for (y in 0 until habitDaySize) {
+                        if (y > 0) habits[x].habitDay.add(HabitDay())
+                        habits[x].habitDay[y].today = input.getOrNull(10 + plusIndexX + plusIndexY)?.toDouble()!!
+                        habits[x].habitDay[y].totalOfAPeriod = input.getOrNull(11 + plusIndexX + plusIndexY)?.toDouble()!!
+                        habits[x].habitDay[y].correctly = input.getOrNull(12 + plusIndexX + plusIndexY).toBoolean()
+                        plusIndexY += 3
+                    }
+                    plusIndexX += 8 + plusIndexY
+                }
+            }
+        }
+    }
 }
