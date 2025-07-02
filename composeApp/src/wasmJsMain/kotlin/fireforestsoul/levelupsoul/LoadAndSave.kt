a153fd7964e5bca7bced66fa37e756c4a4ca2cce
Fire Forest Souls <stepan.sniff@gmail.com>
2025-07-02 09:23:21 +0300

    v1.0.0 Save habit & soul levels on Web
    🌐 Добавлено сохранение параметров уровня привычек и души для Web-версии

--- composeApp/src/wasmJsMain/kotlin/fireforestsoul/levelupsoul/LoadAndSave.kt
+++ composeApp/src/wasmJsMain/kotlin/fireforestsoul/levelupsoul/LoadAndSave.kt
@@ -15,7 +15,12 @@ actual fun saveValue() {
         localStorage.setItem("habits-$x-needDays", habits[x].needDays.toString())
         localStorage.setItem("habits-$x-typeOfColorHabits", habits[x].typeOfColorHabits.toString())
         localStorage.setItem("habits-$x-colorGood", habits[x].colorGood.value.toString(16))
+        localStorage.setItem("habits-$x-changeLevel", habits[x].changeLevel.toString())
+        localStorage.setItem("habits-$x-changeNeedGoalWithLevel", habits[x].changeNeedGoalWithLevel.toString())
+        localStorage.setItem("habits-$x-changeNeedDaysWithLevel", habits[x].changeNeedDaysWithLevel.toString())
         localStorage.setItem("habits-$x-startDate", habits[x].startDate.toString())
+        localStorage.setItem("habits-$x-lastLevelChangeDate", habits[x].lastLevelChangeDate.toString())
+        localStorage.setItem("habits-$x-level", habits[x].level.toString())
         localStorage.setItem("habits-$x-habitDay-size", habits[x].habitDay.size.toString())
         for (y in 0 until habits[x].habitDay.size) {
             localStorage.setItem("habits-$x-habitDay-$y-today", habits[x].habitDay[y].today.toString())
@@ -29,31 +34,16 @@ actual fun saveValue() {
     localStorage.setItem("soul_color_type", soul_color_type.toString())
     localStorage.setItem("soul_color", soul_color.value.toString(16))
     localStorage.setItem("soul_name", soul_name)
+    localStorage.setItem("soul_level", soul_level.toString())
+    localStorage.setItem("soul_last_level_change_date", soul_last_level_change_date.toString())
 }
 
 actual fun loadValue() {
 
-    /** [oldAppVersion]
-     * Check save version
-     *
-     * V > 0 `Habits >`
-     *
-     * V >= 4.000.000 `Soul >`
-     */
     val oldAppVersion = localStorage.getItem("app_version")?.toLong()
     if (oldAppVersion != null) {
         if (oldAppVersion > 0) {
 
-            /** [habitsSize]
-             *
-             * Load Habits:
-             *
-             * `nameOfHabit` `nameOfUnitsOfDimension` `typeOfGoalHabits` `needGoal` `needDays`
-             *
-             * V >= 2.000.000 `typeOfColorHabits` `colorGood`
-             *
-             * `startDate` `habitDays >`
-             */
             val habitsSize = localStorage.getItem("habits-size")?.toInt()!!
             habits = mutableListOf(Habit())
             for (x in 0 until habitsSize) {
@@ -70,19 +60,28 @@ actual fun loadValue() {
                 )
                 habits[x].needGoal = localStorage.getItem("habits-$x-needGoal")?.toDouble()!!
                 habits[x].needDays = localStorage.getItem("habits-$x-needDays")?.toInt()!!
+
                 if (oldAppVersion >= 2000000) {
                     habits[x].typeOfColorHabits =
                         enumValueOf<TypeOfColorHabits>(localStorage.getItem("habits-$x-typeOfColorHabits").toString())
                     habits[x].colorGood = Color(localStorage.getItem("habits-$x-colorGood").toString().toULong(16))
+
+                    if (oldAppVersion >= 1000000000) {
+                        habits[x].changeLevel = localStorage.getItem("habits-$x-changeLevel").toBoolean()
+                        habits[x].changeNeedGoalWithLevel =
+                            localStorage.getItem("habits-$x-changeNeedGoalWithLevel").toBoolean()
+                        habits[x].changeNeedDaysWithLevel =
+                            localStorage.getItem("habits-$x-changeNeedDaysWithLevel").toBoolean()
+                    }
                 }
                 habits[x].startDate = localStorage.getItem("habits-$x-startDate")?.let { LocalDate.parse(it) }!!
 
-                /** [habitDaySize]
-                 *
-                 * Load habit day:
-                 *
-                 * `today` `totalOfAPeriod` `correctly`
-                 */
+                if (oldAppVersion >= 1000000000) {
+                    habits[x].lastLevelChangeDate =
+                        localStorage.getItem("habits-$x-lastLevelChangeDate")?.let { LocalDate.parse(it) }!!
+                    habits[x].level = localStorage.getItem("habits-$x-level")?.toInt()!!
+                }
+
                 val habitDaySize = localStorage.getItem("habits-$x-habitDay-size")?.toInt()!!
                 habits[x].habitDay = mutableListOf(HabitDay())
                 for (y in 0 until habitDaySize) {
@@ -96,13 +95,15 @@ actual fun loadValue() {
             }
 
             if (oldAppVersion >= 4000000) {
-
-                /**
-                 * `soul_color_type` `soul_color` `soul_name`
-                 */
                 soul_color_type = enumValueOf<TypeOfColorHabits>(localStorage.getItem("soul_color_type").toString())
                 soul_color = Color(localStorage.getItem("soul_color").toString().toULong(16))
                 soul_name = localStorage.getItem("soul_name").toString()
+
+                if (oldAppVersion >= 1000000000) {
+                    soul_level = localStorage.getItem("soul_level")?.toInt()!!
+                    soul_last_level_change_date =
+                        localStorage.getItem("soul_last_level_change_date")?.let { LocalDate.parse(it) }!!
+                }
             }
         }
     }
