0d4f4871577e60acdf94b312756e25cd1a9e8682
Fire Forest Souls <stepan.sniff@gmail.com>
2025-07-03 12:02:51 +0300

    v1.0.0 Color depends on level
    🎨 Добавлена возможность изменения цвета привычки в зависимости от её уровня

--- composeApp/src/commonMain/kotlin/fireforestsoul/levelupsoul/ColorsByIndex.kt
+++ composeApp/src/commonMain/kotlin/fireforestsoul/levelupsoul/ColorsByIndex.kt
@@ -2,6 +2,7 @@ package fireforestsoul.levelupsoul
 
 import androidx.compose.ui.graphics.Color
 import kotlin.math.max
+import kotlin.math.min
 
 fun seeColorByIndex(index: Int): Color {
     var maxDays = 0
@@ -15,38 +16,32 @@ fun seeColorByIndex(index: Int): Color {
             maxSeria
         )
     }
+    var maxLevel = 0
+    var minLevel = 0
+    for (habit in habits) {
+        maxLevel = max(maxLevel, habit.level)
+        minLevel = min(minLevel, habit.level)
+    }
+    var levels = maxLevel - minLevel
 
+    /**
+     * red= 2 green= 1 blue= 1
+     */
     return if (habits[index].typeOfColorHabits == TypeOfColorHabits.SELECTED)
         habits[index].colorGood
     else Color(
-        (habits[index].habitDay.size.toDouble() / (if (maxDays != 0) maxDays else 1).toDouble() * 255.0).toInt(),
+        (habits[index].habitDay.size.toDouble() / (if (maxDays != 0) maxDays else 1).toDouble() * 127.5).toInt()
+        + ((habits[index].level - minLevel) / (if (levels == 0) 1 else levels) * 127.5).toInt(),
         (progress(index) * 255.0).toInt(),
         ((if (habitSeria(index).isNotEmpty()) habitSeria(index)[0] else 0) / (if (maxSeria != 0) maxSeria else 1) * 255.0).toInt()
     )
 }
 
 fun noSeeColorByIndex(index: Int): Color {
-    var maxDays = 0
-    for (habit in habits) {
-        maxDays = max(habit.habitDay.size, maxDays)
-    }
-    var maxSeria = 0
-    for (x in 0 until habits.size) {
-        maxSeria = max(
-            if (habitSeria(x).isNotEmpty()) habitSeria(x)[0] else 0,
-            maxSeria
-        )
-    }
-
-    return if (habits[index].typeOfColorHabits == TypeOfColorHabits.SELECTED)
-        Color(
-            habits[index].colorGood.red * 0.5F,
-            habits[index].colorGood.green * 0.5F,
-            habits[index].colorGood.blue * 0.5F
-        )
-    else Color(
-        (habits[index].habitDay.size.toDouble() / (if (maxDays != 0) maxDays else 1).toDouble() * 127.5).toInt(),
-        (progress(index) * 127.5).toInt(),
-        ((if (habitSeria(index).isNotEmpty()) habitSeria(index)[0] else 0) / (if (maxSeria != 0) maxSeria else 1) * 127.5).toInt()
+    val seeColor = seeColorByIndex(index)
+    return Color(
+        seeColor.red * 0.5f,
+        seeColor.green * 0.5f,
+        seeColor.blue * 0.5f
     )
 }
